3fdade448177e97bb894a531a1a082966d6efb80
Kyle Chaplin <kchaplin@gatewayhealthpartners.com>
2015-12-02 18:11:08 -0600

    Fix contact name

--- app/scripts/conversation.js
+++ app/scripts/conversation.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
   ConversationService.getConversation(conversationId).then(function(conversation){
     var messages = conversation.messages;
     for(var i = 0; i < messages.length; i++) {
-      $conversationContainer.append("<p>" + conversation.contactName + " : " + messages[i].message + "</p>");
+      var message = messages[i];
+      $conversationContainer.append("<p>" + message.contactName + " : " + messages[i].message + "</p>");
     }
   });
 
