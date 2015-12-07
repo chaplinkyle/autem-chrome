788aeed5a3d2bb79a3c88c912f194bc660a42c84
Gregory Sarrica <gsarrica@medicalpaymentexchange.com>
2015-12-07 15:56:56 -0500

    add list of conversations

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,16 +1,40 @@
 'use strict';
-
-function testMessage() {
-  var to = document.getElementById('test-to').value
-  var message = document.getElementById('test-message').value
-  chrome.extension.getBackgroundPage().MessageService.sendMessage(to, message);
-}
+var ConversationService;
 
 document.addEventListener('DOMContentLoaded', function () {
-  jQuery('#send-button').on('click', testMessage);
   jQuery('#app-settings-link').on('click', openSettings);
 });
 
 function openSettings() {
   chrome.tabs.create({ url: "settings.html" });
+}
+
+function openConversation(converstaionLink) {
+	chrome.extension.getBackgroundPage().focusedConversationId = jQuery(converstaionLink).attr('id');
+	chrome.windows.create({
+      'url': 'conversation.html',
+      'type': 'popup',
+      'width': 320,
+      'height': 435
+    });
+}
+
+jQuery(document).ready(function(){
+	loadConversations();
+});
+
+function loadConversations() {
+	ConversationService = chrome.extension.getBackgroundPage().ConversationService;
+	var converstationsElement = jQuery('#converstations');
+	ConversationService.getConversations().then(function(conversations){
+	    jQuery(conversations).each(function(index, conversation) {
+	    	var contactName = conversation.messages[conversation.messages.length-1];
+	    	if(typeof contactName == 'undefined') {
+	    		contactName = conversation.contact;
+	    	}
+	    	jQuery(converstationsElement).append('<div id="' + conversation.contact + '"class="conversation"><a href="#">' + conversation.messages[conversation.messages.length-1].contactName + '</a></div');
+		});
+	    // add onclick here?
+ 	});
+
 }
